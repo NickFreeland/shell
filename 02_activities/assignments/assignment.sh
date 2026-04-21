2dff8c55f0f4093e0dc6f686b283e1918cccc253
Nick Freeland <nickfreeland226@gmail.com>
2026-04-21 08:38:19 -0400

    Updated script again

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,7 +36,7 @@ ls ./data/raw
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
 mkdir ./data/processed
-Cd ./data/processed
+cd ./data/processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/*server*.log ./data/processed/server_logs
